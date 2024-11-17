74828c1345c2bc3d873855abf6e1d44be43fe0f1
sumonDev9 <sumonmit678@gmail.com>
2024-11-17 09:08:19 +0530

    firebase install and project setup

---
+++ src/Firebase/Firebase_init.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ0gr31FtdsOZD7Zatu8r_rd0N-L4Cv4w",
+  authDomain: "dragon-news-1dd95.firebaseapp.com",
+  projectId: "dragon-news-1dd95",
+  storageBucket: "dragon-news-1dd95.firebasestorage.app",
+  messagingSenderId: "269999622521",
+  appId: "1:269999622521:web:c68237d53fcf0a164e07a8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
